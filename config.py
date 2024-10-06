1d927989c1d1dd59800941b1a8c817a7eed355fe
Mgyuuy <uuuumithun@gmail.com>
2024-10-07 01:05:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7386860938:AAGEtnAgUHEiW9FkjgLyDu9hv47AEiHJfaw")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@rajaraj")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1383460048")
+
+
+
+
+
+
+
+  
